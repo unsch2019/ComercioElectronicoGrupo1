4a6b6700bbbcff65a0330992302a03ed348d1a32
Richard Zapata <r_zapatac@hotmail.com>
2019-05-31 18:45:52 -0500

    Arquitectura 1.0

---
+++ Negocio/UsuarioBL.cs
@@ -0,0 +1,13 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class UsuarioBL: Repositorio<Usuario>
+    {
+    }
+}
---
+++ Tienda/Controllers/ComunController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tienda.Controllers
+{
+    public class ComunController : Controller
+    {
+        public static string ObtenerEmpresa()
+        {
+            return ConfigurationManager.AppSettings["Empresa"];
+        }
+    }
+}
